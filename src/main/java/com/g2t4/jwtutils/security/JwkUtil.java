04233e7c57246ff857563d91ae1892970077d471
EddyEhhh <54491691+EddyEhhh@users.noreply.github.com>
2024-10-27 23:27:19 +0800

    Update JwkUtil.java

--- src/main/java/com/g2t4/jwtutils/security/JwkUtil.java
+++ src/main/java/com/g2t4/jwtutils/security/JwkUtil.java
@@ -12,9 +12,10 @@ import java.security.interfaces.RSAPublicKey;
 import java.util.concurrent.TimeUnit;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 
-@Component
+@Service
 public class JwkUtil {
 
     @Value("${cognito.userpool.id:not_found}")
