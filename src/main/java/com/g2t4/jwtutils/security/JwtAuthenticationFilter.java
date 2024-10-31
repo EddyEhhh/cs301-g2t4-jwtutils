2a191811103aa5ae9c9271e8c724c560cc1036cc
EddyEhhh <eyyeddy@gmail.com>
2024-11-01 00:30:02 +0800

    test

--- src/main/java/com/g2t4/jwtutils/security/JwtAuthenticationFilter.java
+++ src/main/java/com/g2t4/jwtutils/security/JwtAuthenticationFilter.java
@@ -15,7 +15,6 @@ import java.util.concurrent.TimeUnit;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
